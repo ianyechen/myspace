751e1d1386edf67381a3ea4ec120fed9d71cc74e
ianc999 <44624592+ianc999@users.noreply.github.com>
2019-09-12 21:52:26 -0400

    heroku deploy

---
+++ myspace/config.py
@@ -0,0 +1,7 @@
+class Config: 
+
+    SECRET_KEY = '21e240dc6d91cb627ebb2399a5ca2f31'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    DEVELOPER_KEY = 'AIzaSyA_nX1R5ykvleJ66TlfWFUklmkkXkd1DuU'
+
+
